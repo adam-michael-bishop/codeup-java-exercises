f9a33e06b574de49da2001b73ded74385cc402ed
Adam Bishop <adam.michael.bishop@gmail.com>
2023-02-09 10:33:26 -0600

    Finished Bob exercise

---
+++ src/main/java/Bob.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean done;
+        String statement;
+        do {
+            System.out.println("Say something to Bob...");
+            statement = input.nextLine();
+            System.out.print("Bob says: ");
+
+            if (statement.endsWith("?")) {
+                System.out.println("'Sure.'");
+            }
+            else if (statement.endsWith("!")) {
+                System.out.println("'Whoa, chill out!'");
+            }
+            else if (statement.trim().length() == 0) {
+                System.out.println("'Fine. Be that way!'");
+            } else {
+                System.out.println("'Whatever.'");
+            }
+
+            System.out.print("""
+                    Bob is staring off into space...
+                    Continue the conversation? Y/N
+                    """);
+            done = "n".equalsIgnoreCase(input.nextLine());
+        } while (!done);
+    }
+}
