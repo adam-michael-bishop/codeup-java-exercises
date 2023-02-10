2323db2eacec4bcbdbfca205ad169c2d95403e4f
Adam Bishop <adam.michael.bishop@gmail.com>
2023-02-10 15:03:23 -0600

    Added max amount of tries before game over and random winning/losing messages

--- src/main/java/HighLow.java
+++ src/main/java/HighLow.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 
 public class HighLow {
     private static final Random rand = new Random();
+    private static final int numberOfGuesses = 6;
     private static final int maxNumber = 100;
     private static final int minNumber = 1;
+    private static final String[] winMessages = {
+            "Good guess! Probably just got lucky...",
+            "Nice! Took you long enough...",
+            "Wow! Really didn't expect much from you...",
+            "Finally! I almost fell asleep."
+    };
+    private static final String[] loseMessages = {
+            "HAHAHAHAHAHA!!!! You suck.",
+            "Honestly this is just embarrassing.",
+            "Try turning your monitor on next time. :)",
+            "Game over man, game over!"
+    };
 
     private static int validateInteger(int min, int max, Scanner input) {
         int intToValidate = input.nextInt();
@@ -16,35 +29,72 @@ public class HighLow {
         }
     }
 
-    public static void main(String[] args) {
-        boolean isDonePlaying = false;
-        int superSecretNumber = rand.nextInt(maxNumber) + 1;
-        Scanner input = new Scanner(System.in);
-
+    private static void playGame(int superSecretNumber, Scanner input) {
+        boolean isDonePlaying;
+        int currentGuesses;
         do {
+            currentGuesses = numberOfGuesses;
             System.out.printf("Pick a number between %d and %d: ", minNumber, maxNumber);
+//            System.out.printf("DEV ONLY: %d", superSecretNumber);
             int guessedNumber = validateInteger(minNumber, maxNumber, input);
-
-            while (guessedNumber != superSecretNumber) {
+            while (guessedNumber != superSecretNumber && currentGuesses > 0) {
+                currentGuesses--;
+                if (currentGuesses <= 0) {
+                    break;
+                }
                 if (guessedNumber > superSecretNumber) {
-                    System.out.println("LOWER");
+                    System.out.printf("LOWER%s%n",guessedNumber == 69 ? " (Nice)" : "");
                 }
                 if (guessedNumber < superSecretNumber) {
-                    System.out.println("HIGHER");
+                    System.out.printf("HIGHER%s%n", guessedNumber == 69 ? " (Nice)" : "");
+                }
+                if (currentGuesses == 1) {
+                    System.out.printf("%d try left. No pressure :)%n", currentGuesses);
+                } else {
+                    System.out.printf("%d tries left%n", currentGuesses);
                 }
                 guessedNumber = validateInteger(minNumber, maxNumber, input);
             }
-
-            System.out.println("""
-                    
-                    GOOD GUESS!
-                    
-                    Play again? Y/N
-                    """);
+            if (currentGuesses == 0) {
+                System.out.printf("""
+                        L
+                        Number was %d%s
+                        
+                        %s
+                        
+                        Play again? Y/N
+                        """,superSecretNumber,superSecretNumber == 69 ? " (Nice)" : "",getRandomLoseMessage());
+            } else {
+                System.out.printf("""
+                        W
+                                            
+                        %s
+                                            
+                        Play again? Y/N
+                        """, getRandomWinMessage(currentGuesses));
+            }
             input.nextLine();
             isDonePlaying = "n".equalsIgnoreCase(input.nextLine());
             superSecretNumber = rand.nextInt(maxNumber) + 1;
         } while (!isDonePlaying);
+    }
+
+    private static String getRandomWinMessage(int currentGuesses) {
+        if (currentGuesses == numberOfGuesses) {
+            return "Cheater...";
+        }
+        return winMessages[rand.nextInt(winMessages.length)];
+    }
+
+    private static String getRandomLoseMessage() {
+        return loseMessages[rand.nextInt(loseMessages.length)];
+    }
+
+    public static void main(String[] args) {
+        int superSecretNumber = rand.nextInt(maxNumber) + 1;
+        Scanner input = new Scanner(System.in);
+
+        playGame(superSecretNumber, input);
 
         input.close();
     }
